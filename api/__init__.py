b50722175bc0f02979510ddbe5ee4586459dd0e1
angelocarasig <s3906344@student.rmit.edu.au>
2022-06-30 13:39:15 +1000

    added gunicorn/flask cors for deployment

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,8 @@
 from functools import cache
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_caching import Cache
 from vndb_thigh_highs import VNDB
+
 
 vndb = VNDB()
 
@@ -9,11 +10,11 @@ cache = Cache()
 
 def create_app():
     app = Flask(__name__)
-    
     app.config['CACHE_TYPE'] = 'simple'
     cache.init_app(app)
 
     from .views import api
     app.register_blueprint(api)
-    
+
     return app
+
--- api/views.py
+++ api/views.py
@@ -1,10 +1,14 @@
-from flask import Blueprint, jsonify, request
+from audioop import cross
+from flask import Blueprint, jsonify, request, send_from_directory
 from .models import load_user_data
 from api import cache
+from flask_cors import CORS, cross_origin
 
-api = Blueprint('api', __name__)
+api = Blueprint('api', __name__, static_folder="../../client/build", static_url_path="/")
+cors = CORS(api)
 
 @api.route("/get_user_data", methods=["POST"])
+@cross_origin()
 def get_user_data():
     userID = request.get_json()
     data = get_ulist(userID['userID'])
@@ -14,3 +18,8 @@ def get_user_data():
 def get_ulist(userID):
     user = load_user_data(userID)
     return user
+
+@api.route("/")
+@cross_origin()
+def serve():
+    return send_from_directory(api.static_folder, "index.html")
