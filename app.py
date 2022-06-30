830c941a16693d8a1926759d9f61ad172e8c826d
angelocarasig <s3906344@student.rmit.edu.au>
2022-06-30 19:52:50 +1000

    changes

--- app.py
+++ app.py
@@ -6,20 +6,18 @@ from models import load_user_data
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 cache = Cache()
-
 app.config['CACHE_TYPE'] = 'simple'
 cache.init_app(app)
-CORS(app)
+
 @app.route("/get_user_data", methods=["POST"])
-@cross_origin()
 def get_user_data():
     userID = request.get_json()
     data = get_ulist(userID['userID'])
     return jsonify(data)
 
 @cache.memoize(timeout=86400)       # Assume that it resets per day
-@cross_origin()
 def get_ulist(userID):
     user = load_user_data(userID)
     return user
