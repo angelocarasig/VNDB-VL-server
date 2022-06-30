98b64db955979694b84cb4cf847167d11ce6a917
angelocarasig <s3906344@student.rmit.edu.au>
2022-06-30 17:32:17 +1000

    changes

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
+from audioop import cross
 from functools import cache
 from flask import Flask, jsonify, request
 from flask_caching import Cache
 from models import load_user_data
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cache = Cache()
 
 app.config['CACHE_TYPE'] = 'simple'
 cache.init_app(app)
-
+CORS(app)
 @app.route("/get_user_data", methods=["POST"])
+@cross_origin()
 def get_user_data():
     userID = request.get_json()
     data = get_ulist(userID['userID'])
