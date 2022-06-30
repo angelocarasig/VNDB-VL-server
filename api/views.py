1639bbcf53fe92ece4a397512180ecf96829b2f6
angelocarasig <s3906344@student.rmit.edu.au>
2022-06-30 12:56:54 +1000

    .

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, jsonify, request
+from .models import load_user_data
+from api import cache
+
+api = Blueprint('api', __name__)
+
+@api.route("/get_user_data", methods=["POST"])
+def get_user_data():
+    userID = request.get_json()
+    data = get_ulist(userID['userID'])
+    return jsonify(data)
+
+@cache.memoize(timeout=86400)       # Assume that it resets per day
+def get_ulist(userID):
+    user = load_user_data(userID)
+    return user
